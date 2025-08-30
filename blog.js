18d3cc92787ed3e5bb81120c45927712e5efaa59
tharun <its.tharun018@gmail.com>
2025-08-30 20:10:09 +0530

    Blog page was added

--- blog.js
+++ blog.js
@@ -1,30 +1,3 @@
-// Smooth slideshow animation for .top-bg with multiple images
-const images = [
-	'assests/home-bg.webp',
-	'assests/bg2.webp',
-	'assests/bg3.webp'
-
-];
-
-let current = 0;
-const topBg = document.querySelector('.top-bg');
-
-function showNextTopBg() {
-	topBg.style.opacity = 0;
-	setTimeout(() => {
-		topBg.style.backgroundImage = `url('${images[current]}')`;
-		topBg.style.opacity = 1;
-		current = (current + 1) % images.length;
-	}, 100); // fade duration
-}
-
-if (topBg) {
-	topBg.style.backgroundImage = `url('${images[0]}')`;
-	topBg.style.transition = 'opacity 1s, background-image 1s';
-	topBg.style.opacity = 1;
-	setInterval(showNextTopBg, 2000); // change every 2 seconds
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
@@ -68,3 +41,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
